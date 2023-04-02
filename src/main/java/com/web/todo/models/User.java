86b9b8b90aba2b784b6bda325f17dc47c0d58f05
William Alonso <williamalonso@outlook.com>
2023-04-02 18:14:07 -0300

    :zap: updating User model validations

--- src/main/java/com/web/todo/models/User.java
+++ src/main/java/com/web/todo/models/User.java
@@ -1,5 +1,8 @@
 package com.web.todo.models;
 
+import java.util.List;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,24 +12,93 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 @Entity
 public class User {
 
+    public interface CreateUser {}
+    public interface UpdateUser {}
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // incrementa automaticamente o 'id'
-    @Column(name = "id", unique = true) // Nome da minha coluna. Isso é opcional
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", length = 100, nullable = false, unique = true) // Estamos dizendo que essa coluna vai ter um varchar de tamanho 100 ao invés de 255 padrão. Também estamos dizendo que esse campo/coluna não pode ser nulo.
-    @NotNull
-    @NotEmpty
-    @Size(min = 2, max = 100)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull
-    @NotEmpty
-    @Size(min = 8, max = 60)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
+
+    // private List<Task> tasks = new ArrayList<Task>(); // Um usuário tem uma lista de tarefas para fazer
     
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User other = (User) obj;
+        if(this.id == null)
+            if(other.id != null)
+                return false;
+            else if(!this.id.equals(other.id))
+                return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = result * prime + ( (this.id == null) ? 0 : this.id.hashCode() );
+        return result;
+    }
+
 }
