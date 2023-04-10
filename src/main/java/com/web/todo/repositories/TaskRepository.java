a3d8251186daf6483bfa88c4041fd5e86a4795ea
William Alonso <williamalonso@outlook.com>
2023-04-10 19:19:03 -0300

    :zap: creating TaskRepository

---
+++ src/main/java/com/web/todo/repositories/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.web.todo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+
+import com.web.todo.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+
+    // @Query( value = "SELECT t FROM Task t WHERE t.user.id = :id", nativeQuery = true )
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+    // @Query( value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+}
