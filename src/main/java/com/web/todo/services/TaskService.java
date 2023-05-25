7ab341ee7601dd824a631c5c81583f91654b45e7
William Alonso <williamalonso@outlook.com>
2023-05-25 01:36:24 -0300

    :zap: updating services

---
+++ src/main/java/com/web/todo/services/TaskService.java
@@ -0,0 +1,53 @@
+package com.web.todo.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.todo.models.Task;
+import com.web.todo.models.User;
+import com.web.todo.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private TaskRepository taskRepository;
+
+  public Task findById(Long id) {
+    Optional<Task> task = this.taskRepository.findById(id);
+    return task.orElseThrow(() -> new RuntimeException(
+      "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()
+    ));
+  }
+
+  @Transactional
+  public Task create(Task obj) {
+    User user = this.userService.findById(obj.getUser().getId());
+    obj.setId(null);
+    obj.setUser(user);
+    obj = this.taskRepository.save(obj);
+    return obj;
+  }
+
+  @Transactional
+  public Task update(Task obj) {
+    Task newObj = findById(obj.getId());
+    newObj.setDescription(obj.getDescription());
+    return this.taskRepository.save(newObj);
+  }
+
+  public void delete(Long id) {
+    findById(id);
+    try {
+      this.taskRepository.deleteById(id);
+    } catch(Exception e) {
+      throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+    }
+  }
+}
--- src/main/java/com/web/todo/services/UserService.java
+++ src/main/java/com/web/todo/services/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.web.todo.models.User;
-import com.web.todo.repositories.TaskRepository;
 import com.web.todo.repositories.UserRepository;
 
 @Service
@@ -16,9 +15,6 @@ public class UserService {
 
   @Autowired
   private UserRepository userRepository;
-
-  @Autowired
-  private TaskRepository taskRepository;
 
   public User findById(Long id) {
     Optional<User> user = this.userRepository.findById(id);
@@ -31,7 +27,6 @@ public class UserService {
   public User create(User obj) {
     obj.setId(null);
     obj = this.userRepository.save(obj);
-    this.taskRepository.saveAll(obj.getTasks());
     return obj;
   }
 
